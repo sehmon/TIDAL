766b165ecfaa357b2c29411495519068164f06c7
Diane Liu <dianeliu@u.northwestern.edu>
2017-05-17 15:13:36 -0500

    i think this should work

---
+++ README.md
@@ -0,0 +1,10 @@
+#TIDAL Project
+Sehmon Burnam, Diane Liu, Nicholas Phillips, Nathan Reiff
+
+##Setup Instructions
+To start the server for the first time:
+```
+cd server/
+npm install
+node index.js
+```
--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
-var path = require('path');
-var app = require('express')();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var path = require('path')
+var express = require('express')
+var app = express()
+var server = require('http').createServer(app)
+var io = require('socket.io')(server)
 var bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -9,21 +10,29 @@ app.use(bodyParser.json())
 
 var carMoving = false
 
-io.on('connection', function(){
-  console.log("Client connected");
-});
+io.on('connection', function (socket) {
+  console.log('Client connected')
+  socket.on('add food', function (food) {
+    console.log('added', food.name)
+  })
+})
 
-app.get('/', function(req, res) {
-      res.sendFile(path.join(__dirname+'/index.html'));
-});
+app.use(express.static('static'))
 
-app.get('/status', function(req, res) {
-    res.json({forward: carMoving});
-});
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'))
+})
 
-app.post('/status', function(req, res){
-    carMoving = (req.body.newCarValue == 'true');
-    res.send("Changed carMoving to: " + carMoving.toString());
-});
+app.get('/status', function (req, res) {
+  res.json({forward: carMoving})
+})
 
-server.listen(3000);
+app.post('/status', function (req, res) {
+  carMoving = (req.body.newCarValue === true)
+  res.send('Changed carMoving to: ' + carMoving.toString())
+  console.log('Changed carMoving to: ' + carMoving.toString())
+})
+
+server.listen(3000, function () {
+  console.log('Listening on port 3000...')
+})

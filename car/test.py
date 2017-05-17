f0a85c7321190c64ec0a301080e8c4455ff881e6
yawkity <sehmon3@gmail.com>
2017-05-17 17:27:41 -0500

    Added comments to server-serial port interaction

--- car/test.py
+++ car/test.py
@@ -1,14 +1,22 @@
-import serial
-import requests
+import serial       # Library for communicating with Arduino
+import requests     # Library for communicating with Topcode Server
 
-ser = serial.Serial('/dev/cu.usbmodem1421', 9600)
+# Connect to the Arduino's serial port
 
-running = True
+SERIAL_PORT = '/dev/cu.usbmodem1421'
+BAUD_RATE = 9600
+
+ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
+
+running = True      # Tells car to start moving or not
 if not running:
-    ser.write('e');
+    ser.write('e'); # Tell car to stop
 else:
     while True:
+        # Reach out to the server and grab the newest value of 'carMoving'
         r = requests.get('http://localhost:3000/status')
+
+        # If the server response is true, tell the car to move forward
         if r.json()["forward"]:
             ser.write('w')
         else:

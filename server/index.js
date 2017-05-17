b575489f4e6a0a4561992af5a658751a8e33e661
yawkity <sehmon3@gmail.com>
2017-05-17 12:50:15 -0500

    Added server code to update car variable

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ var path = require('path');
 var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+var carMoving = false
 
 io.on('connection', function(){
   console.log("Client connected");
@@ -9,6 +15,15 @@ io.on('connection', function(){
 
 app.get('/', function(req, res) {
       res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/status', function(req, res) {
+    res.json({forward: carMoving});
+});
+
+app.post('/status', function(req, res){
+    carMoving = (req.body.newCarValue == 'true');
+    res.send("Changed carMoving to: " + carMoving.toString());
 });
 
 server.listen(3000);
